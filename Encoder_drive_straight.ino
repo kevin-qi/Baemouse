dc76fe6b25b281c8e68fc4bafee16ce71a403ecd
kevin-qi <kevin.qi@berkeley.edu>
2016-10-26 21:53:51 -0700

    Add files via upload

--- Encoder_drive_straight.ino
+++ Encoder_drive_straight.ino
@@ -1,10 +1,22 @@
+/*
+ * Notes for circuit building:
+ * - Left is white wheel, Right is black wheel
+ * - LEFT/RIGHT Encoder Pins: A to D2/D3, B to D4/D7 
+ * - LEFT/RIGHT Motor Pins: M1 to 20f/30e, M2 to 19f/29e ~~~ This is opposite (relative to h-bridge) b/c the wheels are mirrored
+ * - LEFT/RIGHT Motor Control Pins: D5/D9 to 22f/27e, D6/D10 to 27f/22e
+ * - Make sure Encoder power pin is connected to 5v and not the battery!!!
+ * - Make sure to ground the arduino with the battery!!! 
+ */
+
 int leftInterrupt=0; //Actualy pin 2
 int leftEncoderPin=4;
 int rightInterrupt=1; //Actually pin 3
 int rightEncoderPin=7;
 
-int leftMotorPin=6;
-int rightMotorPin=9;
+int leftMotorPin1=5; 
+int leftMotorPin2=6;
+int rightMotorPin1=9; 
+int rightMotorPin2=10;
 
 volatile int leftTicks=0;
 volatile int rightTicks=0;
@@ -20,13 +32,13 @@ void setup() {
   pinMode(rightEncoderPin, INPUT);
 
   //Attach Interrupts
-  attachInterrupt(leftInterrupt, onLeftInterrupt, FALLING);
-  attachInterrupt(rightInterrupt, onRightInterrupt, FALLING); 
+  attachInterrupt(leftInterrupt, onLeftInterrupt, RISING);
+  attachInterrupt(rightInterrupt, onRightInterrupt, FALLING); //Falling the wheels are mirrored on the vehicle
 }
 
 void onLeftInterrupt() {
   //We know that pin1 just switched to LOW
-  if(leftEncoderPin==0){ //Check if pin2 is at low as well
+  if(digitalRead(leftEncoderPin)==0){ //Check if pin2 is at low as well
     leftTicks++; //If so, then it is moving forward
   }
   else{
@@ -36,7 +48,7 @@ void onLeftInterrupt() {
 
 void onRightInterrupt() {
   //We know that pin1 just switched to LOW
-  if(rightEncoderPin==0){ //Check if pin2 is at low as well
+  if(digitalRead(rightEncoderPin)==0){ //Check if pin2 is at low as well
     rightTicks++; //If so, then it is moving forward
   }
   else{
@@ -45,20 +57,22 @@ void onRightInterrupt() {
 }
 
 void driveLeft(float spd){
-  analogWrite(leftMotorPin, spd*255);
+  analogWrite(leftMotorPin1, spd*255);
 }
 
 void driveRight(float spd){
-  analogWrite(rightMotorPin, spd*255);
+  analogWrite(rightMotorPin1, spd*255);
 }
 
 int prev_error = 0;
+int prev=0;
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println(rightTicks-leftTicks);
+  Serial.println(prev);
+  prev = rightTicks-leftTicks;
   int error = leftTicks - rightTicks;
   int delta_error = error - prev_error;
-  float P = -1;
+  float P = -2;
   float D = -100;
   float pidOutput = P * error + D * error;
   prev_error = error;
